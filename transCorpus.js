e75f63c7e75a5186decaf0bf575af9a9150c17b8
branpap <brandonpapineau254@gmail.com>
2022-11-30 09:58:34 -0800

    added poster

---
+++ transCorpus.js
@@ -0,0 +1,10 @@
+// function SearchPress(){
+//     fs.readdirSync("transCorpus/BreitbartText").forEach((file) => {
+//         console.log(file)
+//    })
+//   };
+
+// var fs = require('fs')
+
+// fs.readFile('')
+
